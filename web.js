3de695fcd9e4b24034b54d48e112c689bf4143d8
akshatgoyal2 <ubuntu@ip-10-164-21-167.ec2.internal>
2013-07-08 14:19:58 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -4,14 +4,16 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var options = {};
-  options.encoding = 'utf-8';
-  var buf = new Buffer(256);
-  fs.readFileSync('index.html',options, function(err,data){
-    if (err) throw err;
-    //console.log(buf.toString(data));
-    response.send(buf.toString(data));
-  });
+  //var options = {};
+  //options.encoding = 'utf-8';
+  //var buf = new Buffer(256);
+  //fs.readFileSync('index.html',options, function(err,data){
+    //if (err) throw err;
+
+    var buf  = new Buffer(fs.readFileSync('index.html'),'utf-8'); 
+    console.log(buf.toString());
+    response.send(buf.toString());
+  //});
 });
 
 var port = process.env.PORT || 5000;
