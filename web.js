0f062c411d46f6d6fef98ef0ae356ccbf4041b83
akshatgoyal2 <ubuntu@ip-10-164-21-167.ec2.internal>
2013-07-08 14:22:37 +0000

    Remvoing the quotes

--- web.js
+++ web.js
@@ -4,16 +4,11 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //var options = {};
-  //options.encoding = 'utf-8';
-  //var buf = new Buffer(256);
-  //fs.readFileSync('index.html',options, function(err,data){
-    //if (err) throw err;
-
+  
     var buf  = new Buffer(fs.readFileSync('index.html'),'utf-8'); 
-    console.log(buf.toString());
-    response.send(buf.toString());
-  //});
+  
+    response.send(buf.toString('utf-8',1,buf.length-1));
+  
 });
 
 var port = process.env.PORT || 5000;
